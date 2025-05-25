4d3c0b0a59b796ce940f069d361b402a71a4edf8
Michael Carbaugh <michael.s.carbaugh@gmail.com>
2025-05-25 00:36:11 -0500

    Renamed Enumerable.avg() to Enumerable.average(). Updated the implementation to return null for empty list OR sequence of null values. The method now returns the average of non null values if provided a sequence of numeric AND null values. Added tests to cover edge cases. Also added a utility for checking values and added additional tests.

--- __tests__/list/avg.spec.ts
+++ __tests__/list/avg.spec.ts
@@ -17,10 +17,30 @@ describe('list', () => {
       const result = list.avg(item => item.age);
       return expect(result).toEqual(33);
     });
-    it('returns undefined for an empty list', () => {
+    it('returns the average from a list comprised of numbers and nulls, ignoring the nulls', () => {
+      const list = new List([
+        { firstName: 'John', lastName: 'Smith', age: 44 },
+        { firstName: 'Susy', lastName: 'Q', age: 25 },
+        { firstName: 'Jane', lastName: 'Doe', age: 30 },
+        { firstName: 'Bert', lastName: 'Reynolds', age: null },
+      ]);
+      const result = list.avg(item => item.age);
+      expect(result).toEqual(33);
+    });
+    it('returns null for an empty list', () => {
       const list = new List([]);
       const result = list.avg();
-      return expect(result).toEqual(undefined);
+      return expect(result).toEqual(null);
+    });
+    it('returns null for sequence of null values', () => {
+      const list = new List([
+        { firstName: 'John', lastName: 'Smith', age: null },
+        { firstName: 'Susy', lastName: 'Q', age: null },
+        { firstName: 'Jane', lastName: 'Doe', age: null },
+        { firstName: 'Bert', lastName: 'Reynolds', age: null },
+      ]);
+      const result = list.avg(x => x.age);
+      return expect(result).toEqual(null);
     });
   });
 });
