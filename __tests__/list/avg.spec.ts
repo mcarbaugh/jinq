9308b7d0ef7a9022a2b519f8ae713c02f06b9d65
Michael Carbaugh <michael.s.carbaugh@gmail.com>
2025-05-24 22:18:24 -0500

    Fix/resolve test typos (#45)
    * test cleanup
    
    * Resolved typos in tests. Changed name of argument for sum, min, max to selector.
    
    * Added missing test to cover the use of a selector for .avg(), .max(), .min()

--- __tests__/list/avg.spec.ts
+++ __tests__/list/avg.spec.ts
@@ -3,10 +3,19 @@ import { List } from "@mcarbaugh/jinq";
 
 describe('list', () => {
   describe('.avg()', () => {
-    it('returns min of underlying array of numbers', () => {
+    it('returns avg of underlying array of numbers', () => {
       const list = new List([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
       const result = list.avg();
       return expect(result).toEqual(5.5);
+    });
+    it('returns avg from a list of objects using a selector', () => {
+      const list = new List([
+        { firstName: 'John', lastName: 'Smith', age: 44 },
+        { firstName: 'Susy', lastName: 'Q', age: 25 },
+        { firstName: 'Jane', lastName: 'Doe', age: 30 },
+      ]);
+      const result = list.avg(item => item.age);
+      return expect(result).toEqual(33);
     });
     it('returns undefined for an empty list', () => {
       const list = new List([]);
--- __tests__/list/min.spec.ts
+++ __tests__/list/min.spec.ts
@@ -8,6 +8,15 @@ describe('List', () => {
       const result = list.min();
       return expect(result).toEqual(1);
     });
+    it('returns min from a list of objects using a selector', () => {
+      const list = new List([
+        { firstName: 'John', lastName: 'Smith', age: 44 },
+        { firstName: 'Susy', lastName: 'Q', age: 24 },
+        { firstName: 'Jane', lastName: 'Doe', age: 15 },
+      ]);
+      const result = list.min(item => item.age);
+      return expect(result).toEqual(15);
+    });
     it('returns undefined for an empty list', () => {
       const list = new List([]);
       const result = list.min();
